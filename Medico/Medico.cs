ed734c0ad7eeaf3e805a2dae630da1ff8fbf8868
burdiaktri <burdiaktrinidad33@gmail.com>
2023-01-15 19:46:56 -0300

    Add project files.

---
+++ Medico/Medico.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio
+
+{
+    public class Medico
+    {
+
+        private string nombre;
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+
+        }
+
+        public Medico(string nombre)
+        {
+            if(nombre.Length > 50 || nombre.Length == 0)
+            {
+                throw new Exception("El nombre no puede estar vacío o superar los 50 carácteres");
+            } 
+            Nombre = nombre;
+        }
+    }
+}
